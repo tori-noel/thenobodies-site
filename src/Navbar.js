5ae2e469c82c4dfd5bc044da2210acb74d3b2ce2
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-11-21 12:43:42 -0500

    added navbar and some styling

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+            <a class="nav-link" href="#">
+              The Team
+            </a>
+            <a class="nav-link" href="#">
+              Our Mission
+            </a>
+            <a class="nav-link ">Get in Touch</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
